b0cceac58dbfadb165ffc6a39582ef58643aa10a
Edwin Rozario <edwin@boost.co.nz>
2017-03-10 15:36:08 +1300

    Why  : Precompile logo What : Done

--- lib/safety_cone_mountable/engine.rb
+++ lib/safety_cone_mountable/engine.rb
@@ -1,5 +1,9 @@
 module SafetyConeMountable
   class Engine < ::Rails::Engine
     isolate_namespace SafetyConeMountable
+
+    initializer "refinery.assets.precompile" do |app|
+    app.config.assets.precompile += %w(safety_cone_mountable/logo.png)
+  end
   end
 end
