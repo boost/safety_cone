5450483c5f84f60ff8cef490a194ca37141ae9b2
Edwin Rozario <edwin@boost.co.nz>
2017-11-12 15:57:54 +1300

    Movied rembering cones variables to the view

--- app/controllers/safety_cone/cones_controller.rb
+++ app/controllers/safety_cone/cones_controller.rb
@@ -2,9 +2,6 @@ require_dependency 'safety_cone/application_controller'
 
 module SafetyCone
   class ConesController < ApplicationController
-    def index
-      @paths = SafetyCone.paths
-      @features = SafetyCone.features
-    end
+    def index; end
   end
 end
